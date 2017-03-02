fd3742a04188329fed3c515f1b97e037d2dbadc8
Iorin0124 <chako_ru0305@yahoo.co.jp>
2017-03-02 12:22:09 +0900

    いっこ失敗したら次々に失敗していく。モジュールまでgi t Hubに上げてしまったし…もうだめかな

--- day4/express-pug/sample/routes/index.js
+++ day4/express-pug/sample/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var array = [1,2,3,4,5,6,7,8,9,0];
+  var ran = Math.floor(Math.random() * array.length);
+  res.render('index', { title: array[ran] });
 });
 
 module.exports = router;
